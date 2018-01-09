bc6fb5517364f956b328f7bf664ca7cb0ab1caf5
Abdul Hafidz Anshari <aditans88@gmail.com>
2018-01-09 16:33:06 +0700

    sample fastpay client

--- app/Fastpay/Client.php
+++ app/Fastpay/Client.php
@@ -11,12 +11,16 @@ class Client
     protected $uid;
     protected $pin;
     protected $httpclient;
+    protected $method;
+    protected $params;
 
     public function __construct()
     {
         $this->url = config('fastpay.url');
         $this->uid = config('fastpay.uid');
         $this->pin = config('fastpay.pin');
+
+        $this->httpclient = new \GuzzleHttp\Client();
     }
 
     public function init(array $config = [])
@@ -30,8 +34,42 @@ class Client
         return $this;
     }
 
-    public function call(Request $request)
+    public function method($method)
     {
+        $this->method = $method;
+        return $this;  
+    }
+
+    public function params(array $params = [])
+    {
+        $this->params = $params;
+        return $this;
+    }
+
+    public function call()
+    {
+        if(empty($this->method)) {
+            return ['error' => 'Unknown Method'];
+        }
+
+        $xmlmrc_data = xmlrpc_encode_request($this->method, $this->params);
         
+        // $response = $this->httpclient->request('POST', $this->url, [
+        //     'headers' => [
+        //         'Content-Type' => 'text/xml'
+        //     ],
+        //     'body' => $xmlmrc_data
+        // ]);
+
+        // $code = $response->getStatusCode(); // 200
+        // $reason = $response->getReasonPhrase(); // OK
+        // $content = $response->getBody()->getContents();
+
+        // if($code !== 200) {
+        //     return $content;
+        // }
+
+        // return xmlrpc_decode($content);
+        return $xmlmrc_data;
     }
 }
--- app/Http/Controllers/Api/BillerController.php
+++ app/Http/Controllers/Api/BillerController.php
@@ -2,19 +2,52 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
+use App\Fastpay\Client as FastpayClient;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class BillerController extends Controller
 {
+    const FASTPAY_METHOD = [
+        'PULSA' => 'fastpay.pulsa',
+        'GAME'  => 'fastpay.game',
+        'PAYMENT' => 'fastpay.pay',
+        'INQUIRY' => 'fastpay.inq'
+    ];
+
+    protected $fastpayclient;
 
     public function __construct()
     {
-        
+        $this->fastpayclient = new FastpayClient;
     }
 
     public function __invoke(Request $request)
     {
-        return $request->all();
+        $fastpayclient = $this->fastpayclient;
+
+        switch ($request->process_code) {
+            case 'PULSA':
+                    $fastpayclient = $fastpayclient
+                        ->method(self::FASTPAY_METHOD[$request->process_code])
+                        ->params([
+                            'productcode' => $request->product_code,
+                            'client_phone' => $request->phone_number,
+                            'unknown' => '',
+                            'uid' => config('fastpay.uid'),
+                            'pin' => config('fastpay.pin'),
+                            'transaction_id' => random_int(11314,123123123)
+                        ]);
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+
+        $params = [];
+
+        $data = $fastpayclient->call();
+        return $data;
     }
 }
